bcc043caccc4287e422beb6a535123370e4c207f
shidel.dev <joeshidel@gmail.com>
2014-03-17 21:07:11 -0500

    spec for euler problem three

--- spec/eulerSpec.js
+++ spec/eulerSpec.js
@@ -10,12 +10,20 @@ describe("euler problem two", function(){
   });
 });
 
+describe("euler problem three", function(){
+	it("finds the highest prime factor of a number", function(){
+		expect(euler.primeFactor(600851475143)).toBe(6857);
+	})
+})
+
 describe("euler problem four",function(){
 	it("finds largest palindrome for three digits factors",function(){
   	 var palindrome = new euler.palindrome(100,999);
   	 expect(palindrome.largest).toBe(906609);
 	});
 });
+
+
 
 
 
